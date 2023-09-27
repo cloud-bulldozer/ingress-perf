2c849f92b7b3185c22cf0dee8f8100db5eaef378
Raul Sevilla <rsevilla@redhat.com>
2023-09-27 13:31:02 +0200

    Use RequiredDuringSchedulingIgnoredDuringExecution rather than PreferredDuringSchedulingIgnoredDuringExecution
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/runner/types.go
+++ pkg/runner/types.go
@@ -37,10 +37,9 @@ const (
 
 var workerAffinity = &corev1.Affinity{
 	NodeAffinity: &corev1.NodeAffinity{
-		PreferredDuringSchedulingIgnoredDuringExecution: []corev1.PreferredSchedulingTerm{
-			{
-				Weight: 100,
-				Preference: corev1.NodeSelectorTerm{
+		RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+			NodeSelectorTerms: []corev1.NodeSelectorTerm{
+				{
 					MatchExpressions: []corev1.NodeSelectorRequirement{
 						{
 							Key:      "node-role.kubernetes.io/worker",
